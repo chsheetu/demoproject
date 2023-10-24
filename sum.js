9e8b0ef46c864b63c6f0c3d0d7f86bc90ce0d47c
chsheetu <chsheetu@gmail.com>
2023-10-24 18:06:38 -0400

    first

---
+++ sum.js
@@ -0,0 +1,6 @@
+function miit(num3) {
+    var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+    var num3=num1+num2;
+       alert(number(num1)+number(num2));
+}
